a24e8f665a4c4b7be121cbcf446f07083057a0c2
Fletcher Chapin <fletchapin@gmail.com>
2021-01-06 12:37:15 -0800

    Added test for pywin32 installation

--- microscope_automation/tests/test_setup_microscope.py
+++ microscope_automation/tests/test_setup_microscope.py
@@ -49,3 +49,10 @@ def test_setup_microscope(prefs_path, expected_components):
         list(microscope_object.microscope_components_ordered_dict.keys())
         == expected_components
     )
+
+
+@pytest.mark.skipif(skip_all_tests, reason="Exclude all tests")
+def test_import_pywin32():
+    # if pywin32 isn't installed this will throw ImportError and never assert True
+    import win32com.client  # noqa
+    assert True
