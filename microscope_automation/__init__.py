e05291a70e13a65c5cf18fb724330f2296370bec
Fletcher Chapin <fletchapin@gmail.com>
2021-02-02 13:04:02 -0800

    Bump version: 0.0.2 → 0.1.0

--- microscope_automation/__init__.py
+++ microscope_automation/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Fletcher Chapin"
 __email__ = "fletcher.chapin@alleninstitute.org"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.0.2"
+__version__ = "0.1.0"
 
 
 def get_module_version():
